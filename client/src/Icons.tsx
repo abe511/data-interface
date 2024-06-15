0300c219f90a6aff2f40bde8d60c982f8c223d28
Art <absolutus11@gmail.com>
2024-06-15 04:48:31 +0400

    feat: add Close icon

--- client/src/Icons.tsx
+++ client/src/Icons.tsx
@@ -11,3 +11,9 @@ export const AngleDownIcon = (props: SVGProps<SVGSVGElement>) => (
     <path d="m15.233 19.175.754.754 6.035-6.035-.754-.754-5.281 5.281-5.256-5.256-.754.754 3.013 3.013z" />
   </svg>
 );
+
+export const CloseIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" {...props}>
+    <path d="m347.582 134.248 30.17 30.17L286.169 256l91.583 91.583-30.17 30.17-91.583-91.583-91.58 91.583-30.17-30.17 91.581-91.583-91.582-91.58 30.17-30.17L256 225.83l91.583-91.582Z"/>
+  </svg>
+)
