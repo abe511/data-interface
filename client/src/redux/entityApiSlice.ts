a8c7850b2f19e8cf164e8985377a886d87e01065
Art <absolutus11@gmail.com>
2024-06-15 06:28:24 +0400

    fix:

--- client/src/redux/entityApiSlice.ts
+++ client/src/redux/entityApiSlice.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
-console.log("base", BASE_URL);
+
 
 export const entityApi = createApi({
   reducerPath: "entityApi",
